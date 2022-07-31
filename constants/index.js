22f0fea915b5ad0245f70e779ad7f83cc42115b2
odumade <104792577+odumade@users.noreply.github.com>
2022-07-31 17:04:25 +0100

    Initial commit with home and signup screen

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+import icons from "./icons";
+import images from "./images";
+import constants from './constants';
+import theme, { COLORS, SIZES, FONTS } from "./theme";
+
+export { icons, images, theme, COLORS, SIZES, FONTS, constants };
---
+++ screens/Scan.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    Image,
+    TouchableOpacity,
+    Linking
+} from 'react-native';
+import {
+    IconButton,
+    TextButton
+} from '../components';
+import {
+    COLORS,
+    SIZES,
+    FONTS,
+    icons,
+    constants
+} from '../constants';
+
+const Scan = ({ navigation }) => {
+    return (
+        <View>
+            <Text>Hello Scanner</Text>
+        </View>
+    )
+}
+
+export default Scan;
+
